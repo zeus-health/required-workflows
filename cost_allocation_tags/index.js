4f3430065c78a4eac8d6a3fc4a52ab1463f69b64
Kindzerske, Matt <mkindzerske@zushealth.com>
2023-08-15 15:40:46 -0400

    readlines

--- cost_allocation_tags/index.js
+++ cost_allocation_tags/index.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+
 const hcl = require("hcl2-parser")
 
 console.log("### START - cost_allocation_tags/index.js");
 
 // Accept input file,
 // iterate over all files
+
+
+
+
+const filePath = 'TF_FILES.tmp';
+
+const fileContent = fs.readFileSync(filePath, 'utf-8');
+const lines = fileContent.split('\n');
+
+for (const line of lines) {
+    console.log(`Line: ${line}`);
+}
