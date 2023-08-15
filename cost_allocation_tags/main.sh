10a2635fcb187289a6f3cf52e999e6002da9017b
Kindzerske, Matt <mkindzerske@zushealth.com>
2023-08-15 15:10:33 -0400

    iterate

--- cost_allocation_tags/main.sh
+++ cost_allocation_tags/main.sh
@@ -9,3 +9,8 @@ pwd
 ls -lsah
 
 ls -lsah ..
+
+# Second exlclusion is from the workflow and checks code out.
+find ../.. -type f -not -path '*/.*' -not -path './required-workflows/*' -name '*.tf' -print0 > TF_FILES.tmp
+
+cat TF_FILES.tmp
