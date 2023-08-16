9d87364bae25ea3e51b0eaaef178a1af3c4c8689
Kindzerske, Matt <mkindzerske@zushealth.com>
2023-08-16 10:33:31 -0400

    test

--- cost_allocation_tags/index.js
+++ cost_allocation_tags/index.js
@@ -1,21 +1,31 @@
 const fs = require('fs');
 const readlineSync = require('readline-sync');
-
 const hcl = require("hcl2-parser")
 
+
 console.log("### START - cost_allocation_tags/index.js");
-
-// Accept input file,
-// iterate over all files
-
-
 
 
 const filePath = 'TF_FILES.tmp';
 
 const fileContent = fs.readFileSync(filePath, 'utf-8');
-const lines = fileContent.split('\n');
+const terraformFilesFullPath = fileContent.split('\n');
 
-for (const line of lines) {
-    console.log(`Line: ${line}`);
+for (const terraformFilePath of terraformFilesFullPath) {
+    console.log(`Files: ${terraformFilePath}`);
+
+   var buffer = fs.readFileSync(terraformFilePath);
+   // console.log(buffer.toString());
+
+   objectResult = hcl.parseToObject(buffer.toString())
+   console.log(objectResult[0])  
 }
+
+
+// open each file witht he parser
+// hcl inspect
+// add it to the list
+// if list.size>0, exit non-zero and print appropriately.
+
+// TODO
+// enabled the REQ_WORKFLOW_PAT across all Zus.
