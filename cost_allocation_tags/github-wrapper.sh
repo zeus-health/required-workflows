483d4e440f775873a426ba19b6144686d51dd041
Kindzerske, Matt <mkindzerske@zushealth.com>
2023-08-21 11:38:44 -0400

    test

--- cost_allocation_tags/github-wrapper.sh
+++ cost_allocation_tags/github-wrapper.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-if [ ! -f TF_TAGGING_FINDS.tmp ]
+FINDINGS_FILENAME="TF_TAGGING_FINDS.tmp"
+
+if [ ! -f "$FINDINGS_FILENAME" ]
+   cat $FINDINGS_FILENAME
 then
-	echo "TF_TAGGING_FINDS.tmp not found, no findings, skipping script..."
+	echo "TF_TAGGING_FINDS.tmp not found, no findings, skipping script and step ..."
    exit 0
 fi
 
-echo "Installing Python dependencies"
-pip install PyGithub --quiet
-python3 github-pr-comment.py $1 $2
+# echo "Installing Python dependencies"
+# pip install PyGithub --quiet
+# python3 github-pr-comment.py $1 $2
